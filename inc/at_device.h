fdeaa3572bdc52008030100d22c463cbc8d8561e
luliang <806034232@qq.com>
2019-11-25 18:19:52 +0800

    add at device class a9g

--- inc/at_device.h
+++ inc/at_device.h
@@ -53,6 +53,7 @@ extern "C" {
 #define AT_DEVICE_CLASS_MW31           0x07U
 #define AT_DEVICE_CLASS_ESP32          0x08U
 #define AT_DEVICE_CLASS_W60X           0x09U
+#define AT_DEVICE_CLASS_A9G            0x0AU
 
 /* Options and Commands for AT device control opreations */
 #define AT_DEVICE_CTRL_POWER_ON        0x01L
