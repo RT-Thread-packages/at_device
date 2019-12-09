5d6ee4ccc3f69bd65c35deb22ebef8479e8d439a
shao7936626 <228112715@qq.com>
2019-12-09 20:32:02 +0800

    添加air720的代码

--- inc/at_device.h
+++ inc/at_device.h
@@ -54,6 +54,7 @@ extern "C" {
 #define AT_DEVICE_CLASS_ESP32          0x08U
 #define AT_DEVICE_CLASS_W60X           0x09U
 #define AT_DEVICE_CLASS_A9G            0x0AU
+#define AT_DEVICE_CLASS_AIR720         0x0BU
 
 /* Options and Commands for AT device control opreations */
 #define AT_DEVICE_CTRL_POWER_ON        0x01L
