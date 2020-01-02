c7b7cb94d468f0cfc113b2c8cbf6ce7abf4591d6
qiyongzhong0 <917768104@qq.com>
2020-01-02 16:00:32 +0800

    add AT_DEVICE_CLASS_ME3616

--- inc/at_device.h
+++ inc/at_device.h
@@ -56,6 +56,7 @@ extern "C" {
 #define AT_DEVICE_CLASS_A9G            0x0AU
 #define AT_DEVICE_CLASS_BC26           0x0BU
 #define AT_DEVICE_CLASS_AIR720         0x0CU
+#define AT_DEVICE_CLASS_ME3616         0x0DU
 
 /* Options and Commands for AT device control opreations */
 #define AT_DEVICE_CTRL_POWER_ON        0x01L
