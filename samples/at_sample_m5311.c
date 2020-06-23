2b00fb072f04f64fb736de7571fac24eae8b2d2b
LXGMAX <leickwell@hotmail.com>
2020-06-23 11:55:31 +0800

    add m5311 sample and fix

--- samples/at_sample_m5311.c
+++ samples/at_sample_m5311.c
@@ -24,28 +24,28 @@
  
 #include <at_device_m5311.h>
 
-#define LOG_TAG				"at.sample.m5311"
+#define LOG_TAG              "at.sample.m5311"
 #include <at_log.h>
 
-#define M5311_DEVICE_NAME	"nb_m5311"
+#define M5311_DEVICE_NAME    "nb_m5311"
 
 static struct at_device_m5311 nb_m5311 = {
-		M5311_SAMPLE_DEVICE_NAME,
-		M5311_SAMPLE_CLIENT_NAME,
+        M5311_SAMPLE_DEVICE_NAME,
+        M5311_SAMPLE_CLIENT_NAME,
 
-		M5311_SAMPLE_POWER_PIN,
-		M5311_SAMPLE_RECIEVE_BUFF_LEN,
+        M5311_SAMPLE_POWER_PIN,
+        M5311_SAMPLE_RECIEVE_BUFF_LEN,
 };
 
 static int m5311_device_register(void)
 {
-	struct at_device_m5311 *m5311 = &nb_m5311;
+    struct at_device_m5311 *m5311 = &nb_m5311;
 
-	return at_device_register(&(m5311->device),
-								m5311->device_name,
-								m5311->client_name,
-								AT_DEVICE_CLASS_M5311,
-								(void *) m5311);
+    return at_device_register(&(m5311->device),
+                                m5311->device_name,
+                                m5311->client_name,
+                                AT_DEVICE_CLASS_M5311,
+                                (void *) m5311);
 }
 
 INIT_APP_EXPORT(m5311_device_register);
