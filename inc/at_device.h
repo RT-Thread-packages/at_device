22a60c02cf1c51aaf9cd5de6878ea15595e05707
Man, Jianting (Meco) <920369182@qq.com>
2022-01-25 21:05:49 -0500

    update to v2.1.0

--- inc/at_device.h
+++ inc/at_device.h
@@ -26,8 +26,8 @@ extern "C" {
 #include <netdev.h>
 #endif /* RT_USING_NETDEV */
 
-#define AT_DEVICE_SW_VERSION           "2.0.5"
-#define AT_DEVICE_SW_VERSION_NUM       0x20005
+#define AT_DEVICE_SW_VERSION           "2.1.0"
+#define AT_DEVICE_SW_VERSION_NUM       0x20100
 
 /* AT device class ID */
 #define AT_DEVICE_CLASS_ESP8266        0x01U
