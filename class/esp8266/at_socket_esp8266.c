7b9cd8a47391c84912d5808c1953aae9dd18580b
luhuadong <luhuadong@163.com>
2022-11-08 16:05:28 +0800

    fix: struct at_socket_ops * assign error for AT device except ESP8266 (Bug from pr #182)

--- class/esp8266/at_socket_esp8266.c
+++ class/esp8266/at_socket_esp8266.c
@@ -478,7 +478,7 @@ static void esp8266_socket_set_event_cb(at_socket_evt_t event, at_evt_cb_t cb)
     }
 }
 
-static struct at_socket_ops esp8266_socket_ops =
+static const struct at_socket_ops esp8266_socket_ops =
 {
     esp8266_socket_connect,
     esp8266_socket_close,
